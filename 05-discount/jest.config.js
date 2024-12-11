bc4feb2f1ebd9c9458768a582efe0644c23f7760
frykhamn <frykhamnz@gmail.com>
2024-12-11 10:23:18 +0100

    template

--- 05-discount/jest.config.js
+++ 05-discount/jest.config.js
@@ -1,7 +1,6 @@
 // jest.config.js (om du använder type: "module" i package.json kan du skriva export default)
 export default {
     testEnvironment: 'node',
-    transform: {},  // Inga transformeringar, förutsatt att koden redan är giltig ESM
-    extensionsToTreatAsEsm: ['.js'] // Behandla .js-filer som ESM
+    transform: {},  // Inga transformeringar, förutsatt att koden redan är giltig ES
   }
   
